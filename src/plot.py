80a65780a6eedacd48a1211e7f91cbedc24c9a3e
Butadiene <najimi0510@gmail.com>
2023-01-28 01:28:39 +0900

    refactering plot.py

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,72 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+
+# Read in the data from the csv files
+t_data = pd.read_csv('./data/result_t.csv')
+x_data = pd.read_csv('./data/result_x.csv')
+
+# Create figure and axes
+fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(12, 12))
+
+# Plot data on ax1
+ax1.plot(t_data['time'], t_data['v_perp_eV'], label='v_perp_eV')
+ax1.plot(t_data['time'], t_data['v_para_eV'], label='v_para_eV')
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Energy (eV)')
+ax1_2 = ax1.twinx()
+ax1_2.plot(t_data['time'], t_data['pitch_angle'], 'r', label='pitch_angle')
+ax1_2.set_ylabel('Pitch Angle')
+
+# Plot data on ax2 (log scale)
+ax2.semilogx(t_data['time'], t_data['v_perp_eV'], label='')
+ax2.semilogx(t_data['time'], t_data['v_para_eV'], label='')
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Energy (eV)')
+ax2_2 = ax2.twinx()
+ax2_2.semilogx(t_data['time'], t_data['pitch_angle'], 'r', label='')
+ax2_2.set_ylabel('Pitch Angle')
+
+# Plot data on ax3
+ax3.plot(x_data['x'], x_data['v_perp_eV'], label='')
+ax3.plot(x_data['x'], x_data['v_para_eV'], label='')
+ax3.set_xlabel('X (km)')
+ax3.set_ylabel('Energy (eV)')
+ax3_2 = ax3.twinx()
+ax3_2.spines["right"].set_position(("axes", 1.15))
+ax3_2.plot(x_data['x'], x_data['pitch_angle'], 'r', label='')
+ax3_2.set_ylabel('Pitch Angle')
+ax3_3 = ax3.twinx()
+ax3_3.plot(x_data['x'], x_data['relative_density'], 'g', label='')
+ax3_3.set_ylabel('relative_density')
+# Plot data on ax4 (log scale)
+ax4.semilogx(x_data['x'], x_data['v_perp_eV'], label='')
+ax4.semilogx(x_data['x'], x_data['v_para_eV'], label='')
+ax4.set_xlabel('X (km)')
+ax4.set_ylabel('Energy (eV)')
+ax4_2 = ax4.twinx()
+ax4_2.spines["right"].set_position(("axes", 1.15))
+ax4_2.semilogx(x_data['x'], x_data['pitch_angle'], 'r', label='')
+ax4_2.set_ylabel('Pitch Angle')
+ax4_3 = ax4.twinx()
+ax4_3.semilogx(x_data['x'], x_data['relative_density'], 'g', label='relative_density')
+ax4_3.set_ylabel('relative_density')
+
+# Plot data on ax5
+ax5.plot(x_data['x'], x_data['energy'], 'c', label='energy')
+ax5.set_xlabel('X (km)')
+ax5.set_ylabel('Energy (eV)')
+ax5_2 = ax5.twinx()
+ax5_2.plot(x_data['x'], x_data['energy_density'], 'm', label='energy_density')
+ax5_2.set_ylabel('Energy density')
+
+# Plot data on ax6 (log scale)
+
+ax6.semilogx(x_data['x'], x_data['energy'], 'c', label='')
+ax6.set_xlabel('X (km)')
+ax6.set_ylabel('Energy (eV)')
+ax6_2 = ax6.twinx()
+ax6_2.semilogx(x_data['x'], x_data['energy_density'], 'm', label='')
+ax6_2.set_ylabel('Energy density')
 
 # Get parameter from c++
 with open("./data/params.txt") as f:
@@ -13,141 +80,16 @@ electric_field = paramlist[4]
 occur_duration = paramlist[5]
 occur_period = paramlist[6]
 accele_t_max = paramlist[7]
+title_format = "{}, {} mV/m, init_v_para = {} eV, init_v_perp = {} eV, \nmax_v_para_for_resonance = {} eV,Wave occurs for {}\
+    sec during {} sec, \naccleration_time_max {} sec"
+plt.suptitle(title_format.format(ion_name, electric_field, init_v_para_eV, init_v_perp_eV, max_v_para_for_resonance_eV,
+             occur_duration, occur_period, accele_t_max))
+plt.subplots_adjust(wspace=0.5, hspace=0.4)
 
-dft = pd.read_csv('./data/result_t.csv')
-dfx = pd.read_csv('./data/result_x.csv')
-
-
-fig, ((ax11, ax1, ax5), (ax15, ax2, ax6), ) = plt.subplots(2, 3, figsize=(12, 8))
-
-# Graph1
-ax1.set_title('Graph1:Time_plot')
-ax1.plot(dft['time'], dft['v_perp_eV'], 'b-', label='v_perp_eV')
-ax1.plot(dft['time'], dft['v_para_eV'], 'r-', label='v_para_eV')
-ax1.set_xlabel('time(s)')
-ax1.set_ylabel('velocity(eV)')
-
-ax3 = ax1.twinx()
-ax3.set_frame_on(True)
-ax3.patch.set_visible(False)
-pitch_angle = dft['pitch_angle']
-ax3.plot(dft['time'], pitch_angle, 'g-', label='pitch_angle')
-ax3.set_ylabel('pitch_angle(degree)', color='g')
-ax3.yaxis.set_ticks_position('right')
-ax3.yaxis.set_tick_params(labelright=True)
-
-
-# Graph2
-ax2.set_title('Graph2:Time_plot(log(t))')
-ax2.plot(dft['time'], dft['v_perp_eV'], 'b-', label='')
-ax2.plot(dft['time'], dft['v_para_eV'], 'r-', label='')
-ax2.set_xlabel('time(s)')
-# ax2.set_yscale("log")
-ax2.set_xscale("log")
-ax2.set_ylabel('velocity(eV)')
-
-ax4 = ax2.twinx()
-ax4.set_frame_on(True)
-ax4.patch.set_visible(False)
-ax4.plot(dft['time'], pitch_angle, 'g-', label='')
-ax4.set_ylabel('pitch_angle(degree)', color='g')
-ax4.yaxis.set_ticks_position('right')
-ax4.yaxis.set_tick_params(labelright=True)
-
-
-# Graph3
-ax5.set_title('Graph3:x_plot')
-ax5.plot(dfx['x'], dfx['v_perp_eV'], 'b-', label='')
-ax5.plot(dfx['x'], dfx['v_para_eV'], 'r-', label='')
-ax5.set_xlabel('x(km)')
-# ax5.set_yscale("log")
-ax5.set_ylabel('velocity(eV)')
-
-ax7 = ax5.twinx()
-ax7.spines["right"].set_position(("axes", 1.15))
-ax7.set_frame_on(True)
-ax7.patch.set_visible(False)
-relative_density = dfx['relative_density']
-ax7.plot(dfx['x'], relative_density, 'm-', label='relative_density')
-ax7.set_ylabel('relative_density', color='m')
-ax7.yaxis.set_ticks_position('right')
-ax7.yaxis.set_tick_params(labelright=True)
-
-ax9 = ax5.twinx()
-ax9.set_frame_on(True)
-ax9.patch.set_visible(False)
-pitch_angle = dfx['pitch_angle']
-ax9.plot(dfx['x'], pitch_angle, 'g-', label='')
-ax9.set_ylabel('pitch_angle(degree)', color='g')
-ax9.yaxis.set_ticks_position('right')
-ax9.yaxis.set_tick_params(labelright=True)
-
-
-# Graph4
-ax6.set_title('Graph4:x_plot(log(x))')
-ax6.plot(dfx['x'], dfx['v_perp_eV'], 'b-', label='')
-ax6.plot(dfx['x'], dfx['v_para_eV'], 'r-', label='')
-ax6.set_xlabel('x(km)')
-# ax6.set_yscale("log")
-ax6.set_xscale("log")
-ax6.set_ylabel('velocity(eV)')
-
-ax8 = ax6.twinx()
-ax8.spines["right"].set_position(("axes", 1.15))
-ax8.set_frame_on(True)
-ax8.patch.set_visible(False)
-relative_density = dfx['relative_density']
-ax8.plot(dfx['x'], relative_density, 'm-', label='')
-ax8.set_ylabel('relative_density', color='m')
-ax8.yaxis.set_ticks_position('right')
-ax8.yaxis.set_tick_params(labelright=True)
-
-ax10 = ax6.twinx()
-ax10.set_frame_on(True)
-ax10.patch.set_visible(False)
-pitch_angle = dfx['pitch_angle']
-ax10.plot(dfx['x'], pitch_angle, 'g-', label='')
-ax10.set_ylabel('pitch_angle(degree)', color='g')
-ax10.yaxis.set_ticks_position('right')
-ax10.yaxis.set_tick_params(labelright=True)
-
-# Graph5
-ax11.set_title('Graph5:erg_plot')
-ax11.plot(dfx['x'], dfx['energy'], 'y-', label='energy')
-ax11.set_xlabel('x(km)')
-ax11.set_ylabel('Energy(eV)')
-
-ax12 = ax11.twinx()
-ax12.set_frame_on(True)
-ax12.patch.set_visible(False)
-ax12.plot(dfx['x'], dfx['energy_density'], 'c-', label='energy_density')
-ax12.set_ylabel('energy_density()', color='c')
-ax12.set_yscale('log')
-ax12.yaxis.set_ticks_position('right')
-ax12.yaxis.set_tick_params(labelright=True)
-
-# Graph6
-ax15.set_title('Graph6:erg_plot(log)')
-ax15.plot(dfx['x'], dfx['energy'], 'y-', label='')
-ax15.set_xlabel('x(km)')
-ax15.set_xscale('log')
-ax15.set_ylabel('Energy(eV)')
-
-ax16 = ax15.twinx()
-ax16.set_frame_on(True)
-ax16.patch.set_visible(False)
-ax16.plot(dfx['x'], dfx['energy_density'], 'c-', label='')
-ax16.set_ylabel('energy_density()', color='c')
-ax16.set_yscale('log')
-ax16.yaxis.set_ticks_position('right')
-ax16.yaxis.set_tick_params(labelright=True)
-
+file_name_format = "./graphs/{}_{}mVm_init_v_para_{}eV_init_v_perp_{}eV_max_resonance_{}eV_{}sec_per_{}t_max_{}sec.png"
+plt.savefig(file_name_format.format(ion_name, electric_field, init_v_para_eV, init_v_perp_eV, max_v_para_for_resonance_eV,
+            occur_duration, occur_period, accele_t_max))
 
 fig.legend(loc='upper left')
-plt.suptitle(ion_name + ", " + electric_field + 'mV/m,' + ' init_v_para = ' + init_v_para_eV + 'eV, init_v_perp = ' + init_v_perp_eV +
-             'eV, max_v_para_for_resonance = ' + max_v_para_for_resonance_eV + "eV,\nWave occurs for " + occur_duration + "sec during " +
-             occur_period + "sec, " + "accleration_time_max" + accele_t_max + 'sec')
 plt.subplots_adjust(wspace=0.5, hspace=0.4)
-plt.savefig('./graphs/' + ion_name + electric_field + 'mVm_' + 'init_v_para_' + init_v_para_eV + 'eV_init_v_perp_' + init_v_perp_eV +
-            'eV_max_resonance_' + max_v_para_for_resonance_eV + "eV" + occur_duration + "sec_per" + occur_period + "t_max" + accele_t_max + "sec.png")
 plt.show()
