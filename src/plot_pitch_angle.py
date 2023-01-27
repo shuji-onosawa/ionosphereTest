996f4871b862d5e8c94b075eb3442da2bad56e90
Butadiene <najimi0510@gmail.com>
2023-01-27 15:45:04 +0900

    add_log

--- src/plot_pitch_angle.py
+++ src/plot_pitch_angle.py
@@ -1,5 +1,22 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors
+
+# Parameter///////////////////////////
+wantLog = False
+distance_accele_area_up = 4000  # Height from obeservation point to accleration area upper limit(km)
+distance_accele_area_low = 5000  # Height from obeservation point to accleration area lower limit(km)
+# Warining! You have to distance_accele_area_up<distance_accele_area_low
+# Because These are distance *from obseravation area*
+dt = 0.1
+acceleration_time = 20
+# ////////////////////////////////////
+
+
+if distance_accele_area_up > distance_accele_area_low:
+    print('Warining! You have to distance_accele_area_up<distance_accele_area_low')
+    exit()
 
 # Get parameter from c++
 with open("./data/params.txt") as f:
@@ -17,23 +34,16 @@ accele_t_max = paramlist[7]
 # 読み込み
 df = pd.read_csv('./data/result_x.csv', header=0)
 
-focus_height = 8000  # Height from accleration area upper limit(km)
-acclere_range = 7900  # Heigth accleration area(km)
-
-# xがAからBまでのデータのみ抽出
-
-x_min = focus_height - acclere_range
-x_max = focus_height
+x_min = distance_accele_area_up
+x_max = distance_accele_area_low
 
 df = df[(df["x"] >= x_min) & (df["x"] <= x_max)]
 
 pitch_angles = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 
 # tの最小値と最大値
-t_min, t_max = df["time"].min(), df["time"].max()
-
-# dt
-dt = 1
+t_min = df["time"].min()
+t_max = t_min + acceleration_time
 
 # t_minからt_min+dt, t_minからt_min+2*dt, t_minからt_min+3*dt.....t_minから
 # t_min+n*dt, t_min+dtからt_min+n*dt, t_min+2*dtからt_min+n*dt, t_min+3*dtから
@@ -60,34 +70,55 @@ for i in range(len(pitch_angles) - 1):
 t_plot = [i * dt for i in range(2 * n)]
 pitch_angles.pop(len(pitch_angles) - 1)
 
+if wantLog:
+    fig, ((ax1, ax3), (ax2, ax4)) = plt.subplots(2, 2, figsize=(12, 8))
+    # energy_density_sumsをlogでカラープロットします
+    ax3.set_title('Graph3:erg_density(log)')
+    energy_density_sums_nonzero = np.array(energy_density_sums) + 1e-10
+    mappable3 = ax3.pcolormesh(t_plot, pitch_angles, energy_density_sums_nonzero, norm=matplotlib.colors.LogNorm())
+    ax3.set_xlabel('t')
+    ax3.set_ylabel('pitch_angle')
+    ax3bar = fig.colorbar(mappable3, ax=ax3, orientation="vertical", norm=matplotlib.colors.LogNorm())
+    ax3.set_label("erg_density(log)")
+    # ax3bar.setclim(-1,1)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
-
+    # relative_density_sumsをlogでカラープロットします
+    ax4.set_title('Graph4:relative_density(log)')
+    relative_density_sums_nonzero = np.array(relative_density_sums) + 1e-10
+    mappable4 = ax4.pcolormesh(t_plot, pitch_angles, relative_density_sums_nonzero, norm=matplotlib.colors.LogNorm())
+    ax4.set_xlabel('time')
+    ax4.set_ylabel('pitch_angle')
+    ax4bar = fig.colorbar(mappable4, ax=ax4, orientation="vertical", norm=matplotlib.colors.LogNorm())
+    # ax1bar.setclim(-1,1)
+    ax4.set_label("density(log)")
+else:
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
 
 # energy_density_sumsをカラープロットします
 ax1.set_title('Graph1:erg_density')
 mappable1 = ax1.pcolormesh(t_plot, pitch_angles, energy_density_sums)
-ax1.set_xlabel('t_min + n*dt')
+ax1.set_xlabel('t')
 ax1.set_ylabel('pitch_angle')
 ax1bar = fig.colorbar(mappable1, ax=ax1, orientation="vertical")
 # ax1bar.setclim(-1,1)
 ax1.set_label("erg_density")
 
 # relative_density_sumsをカラープロットします
-ax1.set_title('Graph2:relative_density')
+ax2.set_title('Graph2:relative_density')
 mappable2 = ax2.pcolormesh(t_plot, pitch_angles, relative_density_sums)
-ax2.set_xlabel('t_min + n*dt')
+ax2.set_xlabel('time')
 ax2.set_ylabel('pitch_angle')
-ax1bar = fig.colorbar(mappable2, ax=ax2, orientation="vertical")
+ax2bar = fig.colorbar(mappable2, ax=ax2, orientation="vertical")
 # ax1bar.setclim(-1,1)
-ax1.set_label("density")
+ax2.set_label("density")
 
 
 plt.suptitle(ion_name + ", " + electric_field + 'mV/m,' + ' init_v_para = ' + init_v_para_eV + 'eV, init_v_perp = ' + init_v_perp_eV +
              'eV, max_v_para_for_resonance = ' + max_v_para_for_resonance_eV + "eV,\nWave occurs for " + occur_duration + "sec during " +
-             occur_period + "sec, " + "accleration_time_max" + accele_t_max + 'sec, ' + 'x_max = ' + str(x_max) + 'km x_min = ' + str(x_min) + 'km')
+             occur_period + "sec, " + "accleration_time_max" + accele_t_max + 'sec, ' + 'accle_area: ' + str(x_min) + '-' + str(x_max) +
+             'km' + 'accle_time ' + str(acceleration_time) + 's')
 plt.subplots_adjust(wspace=0.5, hspace=0.4)
 plt.savefig('./graphs/pitch_angle' + ion_name + electric_field + 'mVm_' + 'init_v_para_' + init_v_para_eV + 'eV_init_v_perp_' + init_v_perp_eV +
             'eV_max_resonance_' + max_v_para_for_resonance_eV + "eV" + occur_duration + "sec_per" + occur_period + "t_max" + accele_t_max +
-            "sec" + 'x_max_' + str(x_max) + 'x_min_' + str(x_min) + '.png')
+            "sec" + 'area_' + str(x_max) + '-' + str(x_min) + 't_max_' + str(acceleration_time) + '.png')
 plt.show()
